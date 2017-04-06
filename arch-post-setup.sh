264dddd12b7f1216151222178b840f5758baa233
W. Alex Chen <alex81527@gmail.com>
2017-04-06 21:09:56 +0800

    Update arch-post-setup.sh

--- arch-post-setup.sh
+++ arch-post-setup.sh
@@ -11,26 +11,32 @@ package="xorg-server xorg-xinit deepin deepin-screenshot\
 
 
 echo 'Packages to be installed:'
-echo '====================================================='
-printf "%s\n" $package
-echo '====================================================='
-sudo pacman -Sy
+echo '================================================================='
+echo -e $package
+echo '================================================================='
+
+# when SIGINT received, exit directly
+sudo pacman -Sy || exit 1
 sudo pacman -S --color auto --noconfirm --needed $package
 
-echo '====================================================='
+echo '================================================================='
 echo 'Fetching .vimrc config file...'
-curl -sSL https://raw.github.com/alex81527/configs/master/.vimrc -o ~/.vimrc
+curl -sSL https://raw.githubusercontent.com/alex81527/configs/master/.vimrc -o ~/.vimrc
+echo '[~/.vimrc] updated.'
 
 echo 'Fetching .xinitrc config file...'
-curl -sSL https://raw.github.com/alex81527/configs/master/.xinitrc -o ~/.xinitrc
+curl -sSL https://raw.githubusercontent.com/alex81527/configs/master/.xinitrc -o ~/.xinitrc
+echo '[~/.xinitrc] updated.'
 
 echo 'Fetching .zshrc config file...'
-curl -sSL https://raw.github.com/alex81527/configs/master/.zshrc -o ~/.zshrc
+curl -sSL https://raw.githubusercontent.com/alex81527/configs/master/.zshrc -o ~/.zshrc
+echo '[~/.zshrc] updated.'
 
 echo 'Getting cscope plugin for vim...'
 mkdir -p ~/.vim/plugin
 curl -sSL http://cscope.sourceforge.net/cscope_maps.vim \
     -o ~/.vim/plugin/cscope_maps.vim
+echo '[~/.vim/plugin/cscope_maps.vim] updated.'
 
-echo '====================================================='
+echo '================================================================='
 echo 'You are all set. Enjoy!'
