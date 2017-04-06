e5bf8b1e76d17d05db61093d13bc1ca04bd87325
W. Alex Chen <alex81527@gmail.com>
2017-04-06 20:47:35 +0800

    Update arch-post-setup.sh
    Packages added: redshift, ethtool, traceroute, and gnu-netcat.
    Configs added: cscope plugin for vim.

--- arch-post-setup.sh
+++ arch-post-setup.sh
@@ -5,8 +5,9 @@ package="xorg-server xorg-xinit deepin deepin-screenshot\
 	xfce4-terminal \
 	zsh git vim rsync openssh most htop cscope \
 	chromium curl \
-	ethtool traceroute ifconfig netstat nc route \
-	gimp"
+	ethtool traceroute gnu-netcat\
+	gimp \
+    redshift"
 
 
 echo 'Packages to be installed:'
@@ -16,6 +17,7 @@ echo '====================================================='
 sudo pacman -Sy
 sudo pacman -S --color auto --noconfirm --needed $package
 
+echo '====================================================='
 echo 'Fetching .vimrc config file...'
 curl -sSL https://raw.github.com/alex81527/configs/master/.vimrc -o ~/.vimrc
 
@@ -25,4 +27,10 @@ curl -sSL https://raw.github.com/alex81527/configs/master/.xinitrc -o ~/.xinitrc
 echo 'Fetching .zshrc config file...'
 curl -sSL https://raw.github.com/alex81527/configs/master/.zshrc -o ~/.zshrc
 
-echo 'Congratulations! You are all set. Enjoy!'
+echo 'Getting cscope plugin for vim...'
+mkdir -p ~/.vim/plugin
+curl -sSL http://cscope.sourceforge.net/cscope_maps.vim \
+    -o ~/.vim/plugin/cscope_maps.vim
+
+echo '====================================================='
+echo 'You are all set. Enjoy!'
